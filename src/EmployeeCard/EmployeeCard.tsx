6ae65bfb1429c767ca560566b8c2cee0caf92544
Jonathan Sandgren <jonathansandg@gmail.com>
2021-11-16 17:03:45 +0100

    Added social media links and styled employee cards and grid list

--- src/EmployeeCard/EmployeeCard.tsx
+++ src/EmployeeCard/EmployeeCard.tsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faGithubSquare,
+  faLinkedin,
+  faTwitterSquare,
+} from '@fortawesome/free-brands-svg-icons';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './EmployeeCard.module.scss';
 import { EmployeeAPIData } from 'api';
@@ -8,7 +15,49 @@ type EmployeeCardProps = {
 };
 
 export const EmployeeCard = ({ data }: EmployeeCardProps) => {
+  const { name, office, imagePortraitUrl, gitHub, linkedIn, twitter } = data;
+
   return (
-    <div className={styles.container}>{`${data.name} - ${data.office}`}</div>
+    <div className={styles.container}>
+      <img className={styles.image} src={imagePortraitUrl!} alt={name} />
+      <div className={styles.info}>
+        <div>
+          <p className={styles.name}>{name}</p>
+          <p className={styles.office}>
+            <FontAwesomeIcon icon={faMapMarkerAlt} />
+            {office}
+          </p>
+        </div>
+        <div className={styles.social}>
+          {gitHub && (
+            <a
+              href={`https://github.com/${gitHub}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faGithubSquare} />
+            </a>
+          )}
+          {twitter && (
+            <a
+              href={`https://twitter.com/${twitter}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faTwitterSquare} />
+            </a>
+          )}
+          {linkedIn && (
+            <a
+              href={`https://linkedin.com${linkedIn}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,9 @@ const API_URL = 'https://api.1337co.de/v3/employees';
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 if (!API_KEY) {
-  throw new Error('Please provide an api key in environment variable REACT_APP_API_KEY');
+  throw new Error(
+    'Please provide an api key in environment variable REACT_APP_API_KEY'
+  );
 }
 
 export interface EmployeeAPIData {
@@ -19,8 +21,8 @@ export const getAllEmployees = async (): Promise<EmployeeAPIData[]> => {
   const res = await fetch(API_URL, {
     method: 'GET',
     headers: {
-      Authorization: API_KEY
-    }
-  })
+      Authorization: API_KEY,
+    },
+  });
   return res.json();
-}
+};
